61c92239186b0357452e06b7c95cd6d4658d20d7
lzt <490845291@qq.com>
2025-05-09 10:47:09 +0800

    1

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,10 +1,15 @@
+import axios from 'axios'
+import { ElButton } from 'element-plus'
 import { defineComponent, Fragment } from 'vue'
 
 export default defineComponent({
     setup(props, { slots, expose, emit, attrs }) {
         return () => (
             <div>
-                hello world
+                <div>hello world</div>
+                <ElButton onClick={() => {
+                    axios.post('http://localhost:7401/test')
+                }}>请求测试</ElButton>
             </div>
         )
     }
